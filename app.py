cbeaae174352611f7bd21179bf9363b3bbb3573b
Jorge Gallegos <gallegosjorge403@gmail.com>
2019-11-01 17:04:06 -0700

    Added odd_lines_printed function

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def lines_printed_backwards(string_list):
 
 def lines_printed_random(string_list):
 
-    '''randomly selects lines from a list of strings and 
+    '''Randomly selects lines from a list of strings and 
         prints them out in random order'''
         
     lines = []
@@ -24,6 +24,13 @@ def lines_printed_random(string_list):
 
     print("\n".join(lines))
 
+def odd_lines_printed(string_list):
+    '''Takes in a list of strings and prints only the
+        odd numbered lines'''
+    for index, line in enumerate(string_list):
+        if (index + 1) % 2 == 1:
+            print(f"{index + 1} {line}")
+
 poem = '''If you are a dreamer, come in,
 If you are a dreamer, a wisher, a liar,
 A hope-er, a pray-er, a magic bean buyer…
@@ -32,5 +39,6 @@ For we have some flax-golden tales to spin.
 Come in!
 Come in!'''.split("\n")
 
-lines_printed_backwards(poem)
-lines_printed_random(poem)
+# lines_printed_backwards(poem)
+# lines_printed_random(poem)
+# odd_lines_printed(poem)
